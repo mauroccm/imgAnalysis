0217a8f89ce7ec5789d4431eab156c149aab7352
Mauro Cafundo de Morais <mauroccm@users.noreply.github.com>
2019-09-19 11:55:11 -0300

    Delete AutoCount_Li.sh

--- AutoCount_Li.sh
+++
@@ -1,9 +0,0 @@
-# AutoCount_Li.sh
-# Sctip para executar AutoCount_Li.ijm
-
-INDIR=/home/mauromorais/R/imgAnalysis/scripts
-
-${HOME}/Fiji.app/ImageJ-linux64 --headless -macro ${INDIR}/AutoCount_Li.ijm
-
-echo "JOB DONE!"
-echo "---------"
